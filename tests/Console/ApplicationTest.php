7cce1c3ea3f927a4b53b1fa88455edbb40cb6a5f
slince <taosikai@yeah.net>
2017-03-15 00:09:57 +0800

    add application test

---
+++ tests/Console/ApplicationTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Slince\Crm\Tests\Console;
+
+use Slince\Crm\Console\Application;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $application = new Application();
+        $this->assertInstanceOf(\Symfony\Component\Console\Application::class, $application);
+        $this->assertEquals(Application::NAME, $application->getName());
+    }
+}
