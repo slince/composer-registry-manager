e79602e13f660bd9a7054388cbadb44d6d203dee
slince <taosikai@yeah.net>
2018-04-04 17:21:45 +0800

    add testcase

---
+++ tests/Command/RepoCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Slince\Crm\Tests\Command;
+
+use Slince\Crm\Command\AddCommand;
+use Slince\Crm\Command\ListCommand;
+use Slince\Crm\Command\RepoCommand;
+use Slince\Crm\Tests\Stub\RepositoryManagerStub;
+use Symfony\Component\Console\Exception\RuntimeException;
+
+class RepoCommandTest extends CommandTestCase
+{
+    protected static $logo = <<<EOT
+ _____   _____        ___  ___  
+/  ___| |  _  \      /   |/   | 
+| |     | |_| |     / /|   /| | 
+| |     |  _  /    / / |__/ | | 
+| |___  | | \ \   / /       | | 
+\_____| |_|  \_\ /_/        |_| 
+EOT;
+
+    public function testExecute()
+    {
+        $manager = new RepositoryManagerStub();
+        $this->assertContains(static::$logo, $this->runCommandTester(new RepoCommand($manager), []));
+    }
+}
